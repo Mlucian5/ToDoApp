62141dde0f361797a46532e712165ed9433a7481
Mlucian5 <moise.lucianioan@yahoo.com>
2023-11-18 20:35:57 +0200

    added bulk deletion and formatted code

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,78 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
-
-  const [showConfirmation, setShowConfirmation] = useState(false);
-  const [todoIdToDelete, setTodoIdToDelete] = useState(null);
-
-  const handleDeleteClick = (id) => {
-    setTodoIdToDelete(id);
-    setShowConfirmation(true);
-  };
-
-  const handleDeleteConfirmed = () => {
-    deleteTodo(todoIdToDelete);
-  };
-  
-  const handleCancelDelete = () => {
-    setShowConfirmation(false);
-  };
-
-  const handleToggleComplete = () => {
-    toggleComplete(task.id);
-  };
-
+export const Todo = ({ task, editTodo, toggleComplete, handleDeleteClick }) => {
 
   return (
     <div className="Todo">
-        <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={handleToggleComplete}>{task.task}</p>
-        <div>
+      <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+      <div>
         <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)} />
         <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => handleDeleteClick(task.id)} />
-        </div>
-        {showConfirmation && (
-        <div className="modal"
-          style={{
-            position: 'fixed',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            backgroundColor: 'green',
-            padding: '20px',
-            borderRadius: '8px',
-            boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-            zIndex: 1000,
-            textAlign: 'center',
-          }}
-        >
-          <p>Are you sure you want to perform this action?</p>
-          <button 
-            style={{
-            backgroundColor: '#e74c3c',
-            color: '#fff',
-            padding: '6px 13px',
-            borderRadius: '4px',
-            cursor: 'pointer',
-            marginRight: '10px',
-            border: 'none',
-          }}
-           onClick={handleDeleteConfirmed}>Yes</button>
-          <button
-            style={{
-              backgroundColor: '#e74c3c',
-              color: '#fff',
-              padding: '6px 13px',
-              borderRadius: '4px',
-              cursor: 'pointer',
-              marginRight: '10px',
-              border: 'none',
-            }}
-           onClick={handleCancelDelete}>No</button>
-        </div>
-      )}
+      </div>
     </div>
   )
 }
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Todo } from "./Todo";
 import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
@@ -10,6 +10,9 @@ export const TodoWrapper = () => {
   const [snackbarTimeout, setSnackbarTimeout] = useState(null);
   const [showDone, setShowDone] = useState(false);
   const [showNotDone, setShowNotDone] = useState(false);
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [todoIdToDelete, setTodoIdToDelete] = useState(null);
+  const [isBulkDeletion, setBulkDeletion] = useState(false);
 
   const filteredTodos = todos.filter(todo => {
     if (showDone && showNotDone) {
@@ -31,6 +34,15 @@ export const TodoWrapper = () => {
 
   const deleteTodo = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
+    notifyDeletionSucess();
+  };
+
+  const deleteToDoBulk = () => {
+    setTodos(todos.filter((todo) => !todo.completed));
+    notifyDeletionSucess();
+  }
+
+  const notifyDeletionSucess = () => {
     setShowSnackbar(true);
 
     // Clear the existing timeout if there is one
@@ -43,7 +55,7 @@ export const TodoWrapper = () => {
 
     // Save the timeout ID for cleanup
     setSnackbarTimeout(timeout);
-  };
+  }
 
   const toggleComplete = (id) => {
     setTodos(
@@ -73,18 +85,44 @@ export const TodoWrapper = () => {
     setShowDone(!showDone);
     setShowNotDone(false);
   };
-  
+
   const handleToggleShowNotDone = () => {
     setShowNotDone(!showNotDone);
     setShowDone(false);
   };
 
-    // Clear the Snackbar timeout if the component unmounts
-    useEffect(() => {
-      return () => {
-        clearTimeout(snackbarTimeout);
-      };
-    }, [snackbarTimeout]);
+
+  const handleDeleteClick = (id) => {
+    setTodoIdToDelete(id);
+    setShowConfirmation(true);
+  };
+
+  const handleDeleteConfirmed = () => {
+    console.log('isBulkDeletion', isBulkDeletion);
+    if (isBulkDeletion) {
+      deleteToDoBulk();
+    } else {
+      deleteTodo(todoIdToDelete);
+    }
+    setShowConfirmation(false);
+  };
+
+  const handleCancelDelete = () => {
+    setShowConfirmation(false);
+  };
+
+  const handleDeleteDone = () => {
+    setBulkDeletion(true);
+    setShowConfirmation(true);
+  };
+
+  // Clear the Snackbar timeout if the component unmounts
+  useEffect(() => {
+    return () => {
+      clearTimeout(snackbarTimeout);
+      setBulkDeletion(false);
+    };
+  }, [snackbarTimeout]);
 
   return (
     <div className="TodoWrapper">
@@ -98,14 +136,57 @@ export const TodoWrapper = () => {
           <Todo
             key={todo.id}
             task={todo}
-            deleteTodo={deleteTodo}
             editTodo={editTodo}
             toggleComplete={toggleComplete}
+            handleDeleteClick={handleDeleteClick}
           />
         )
       )}
-       {showSnackbar && (
-          <div className="snackbar"
+
+      {showConfirmation && (
+        <div className="modal"
+          style={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: 'green',
+            padding: '20px',
+            borderRadius: '8px',
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+            zIndex: 1000,
+            textAlign: 'center',
+          }}
+        >
+          <p>Are you sure you want to perform this action?</p>
+          <button
+            style={{
+              backgroundColor: '#e74c3c',
+              color: '#fff',
+              padding: '6px 13px',
+              borderRadius: '4px',
+              cursor: 'pointer',
+              marginRight: '10px',
+              border: 'none',
+            }}
+            onClick={handleDeleteConfirmed}>Yes</button>
+          <button
+            style={{
+              backgroundColor: '#e74c3c',
+              color: '#fff',
+              padding: '6px 13px',
+              borderRadius: '4px',
+              cursor: 'pointer',
+              marginRight: '10px',
+              border: 'none',
+            }}
+            onClick={handleCancelDelete}>No</button>
+        </div>
+      )}
+
+
+      {showSnackbar && (
+        <div className="snackbar"
           style={{
             display: 'inline-block',
             padding: '10px 20px',
@@ -117,23 +198,37 @@ export const TodoWrapper = () => {
             borderRadius: '4px',
             boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)'
           }}
-          >
-            Element deleted successfully!
-          </div>
-       )}
-       <button
+        >
+          Element deleted successfully!
+        </div>
+      )}
+      <button
         style={{
-          backgroundColor: '#e74c3c',
+          backgroundColor: 'green',
           color: '#fff',
           padding: '6px 13px',
           borderRadius: '4px',
           cursor: 'pointer',
           marginRight: '10px',
           border: 'none',
-        }} 
+        }}
         onClick={handleToggleShowDone}>
         {showDone ? 'Show All' : 'Show Done Only'}
       </button>
+      <button
+        style={{
+          backgroundColor: 'purple',
+          color: '#fff',
+          padding: '6px 13px',
+          borderRadius: '4px',
+          cursor: 'pointer',
+          marginRight: '10px',
+          border: 'none',
+        }}
+        onClick={handleToggleShowNotDone}>
+        {showNotDone ? 'Show All' : 'Show Not Done Only'}
+      </button>
+
       <button
         style={{
           backgroundColor: '#e74c3c',
@@ -144,9 +239,10 @@ export const TodoWrapper = () => {
           marginRight: '10px',
           border: 'none',
         }}
-        onClick={handleToggleShowNotDone}>
-        {showNotDone ? 'Show All' : 'Show Not Done Only'}
-      </button> 
+        onClick={handleDeleteDone}
+      >
+        Delete Done
+      </button>
     </div>
   );
 };
